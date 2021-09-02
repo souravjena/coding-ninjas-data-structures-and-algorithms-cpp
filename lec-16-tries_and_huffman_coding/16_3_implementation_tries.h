8a62e1d7fbe680e5f561f025ebb4731241858b8a
Sourav Jena <sourav.jena.mail@gmail.com>
2021-09-02 17:03:58 +0530

    tries implementation added

---
+++ lec-16-tries_and_huffman_coding/16_3_implementation_tries.h
@@ -0,0 +1,141 @@
+#include "16_3_implementation_tries_node.h"
+#include <string>
+using namespace std;
+
+class Trie{
+
+	private:
+		TrieNode* root;
+
+	public:
+		Trie(){
+
+			root = new TrieNode('\0');
+
+		}
+
+
+		// ** important **
+		void insertWord(TrieNode* curr_node, string word){
+
+			// 1. Base Case
+			if(word.size() == 0){
+				curr_node->isTerminal = true;
+				return;
+			}
+
+
+			// 2. Current Problem
+			int index_char = word[0] - 'a';		// Assuming all input words are in lowercase.
+
+			TrieNode* child_node = curr_node->children[index_char];
+
+			if(child_node == NULL){
+				TrieNode* new_node = new TrieNode(word[0]);
+				curr_node->children[index_char] = new_node;
+				child_node = new_node;
+			}
+
+
+			// 3. Recursive Case
+			insertWord(child_node, word.substr(1));
+
+		}
+
+		void insertWord(string word){
+			insertWord(this->root, word);
+		}
+
+		
+		
+		// ** important **
+		bool search(TrieNode* curr_node, string word){
+
+			// 1. Base Case
+			if(word.size() == 0){
+				// This means that the word ends here.
+				// So we need to check if the current node is a terminal node or not.
+				if(curr_node->isTerminal == true){
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			
+			// 2. Current Problem
+			int index_char = word[0] - 'a';		// Assuming all input words are in lowercase.
+
+			TrieNode* child_node = curr_node->children[index_char];
+
+			if(child_node == NULL){
+				return false;
+			} 
+				
+			
+			// 3. Recursive Case
+			bool ans = search(child_node, word.substr(1));
+			return ans;
+		}
+
+		bool search(string word){
+			return search(this->root, word);
+		}
+
+
+
+		// ** important **
+		void removeWord(TrieNode* curr_node, string word){
+
+			// 1. Base Case
+			if(word.size() == 0){
+				// Make the final char of the word to be deleted, non-terminating
+				curr_node->isTerminal = false;
+				return;
+			}
+
+
+			// 2. Current Problem
+			int child_index = word[0] - 'a';
+
+			TrieNode* child_node = curr_node->children[child_index];
+
+			if(child_node == NULL){
+				return;
+				// This indicates that the word to be removed is not there in the trie.
+			}
+
+
+			// 3. Recursive Case
+			removeWord(child_node, word.substr(1));
+
+
+			// 4. Post Recursion
+			/*
+			- Now that isTerminal flag is removed from terminating node
+			- we can delete the child node if it is not a terminating node for some other word and
+			- it does not have any children which would otherwise suggest that this node 
+			  is part of some other word in the trie.
+			*/
+
+			if(child_node->isTerminal == false){
+
+				for(int i = 0; i < 26; i++){
+					if(child_node->children[i] != NULL){
+						return;
+					}
+				}
+
+				delete child_node;
+
+				curr_node->children[child_index] = NULL;
+
+			}
+
+		}
+
+		void removeWord(string word){
+			removeWord(this->root, word);
+		}
+
+};
---
+++ lec-16-tries_and_huffman_coding/16_3_implementation_tries_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "16_3_implementation_tries.h"
+using namespace std;
+
+
+int main(){
+	
+	Trie t;
+	t.insertWord("and");
+	t.insertWord("are");
+	t.insertWord("dot");
+
+	cout << t.search("and") << endl;
+
+	t.removeWord("and");
+	cout << t.search("and") << endl;
+
+
+	return 0;
+}
